2327778966dab16eb323218da540dd76b5328558
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-07-24 23:49:57 +0300

    Interim Commit 24072022

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
+// import * as basicLightbox from 'basiclightbox';  
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>
+//             Your first lightbox with just a few lines of code.
+//             Yes, it's really that simple.
+//         </p>
+//     </div>
+// `)
+
+// instance.show() 
