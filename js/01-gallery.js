aff677ac3a101b8554a316060e11a81f275c8e90
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-07-25 19:18:56 +0300

    Interim Commit 25.07.2022 (task 1/2))

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,65 @@
 import { galleryItems } from './gallery-items.js';
-// import * as basicLightbox from 'basiclightbox';  
+ 
 // Change code below this line
 
-console.log(galleryItems);
+// const galleryContainer = document.querySelector(".gallery");
+// const galleryToInclude = galleryItems.map(item => `<div class="gallery__item">
+//     <a class="gallery__link" href="${item.original}">
+//         <img class="gallery__image"
+//             src="${item.preview}"
+//             data-source="${item.original}"
+//             alt="${item.description}"
+//         />
+//     </a>
+// </div>`).join("");
+
+// galleryContainer.innerHTML = galleryToInclude;
+
+
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryToInclude = galleryItems.map(item => `<div class="gallery__item">
+    
+        <img class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+        />
+    
+</div>`).join("");
+
+galleryContainer.innerHTML = galleryToInclude;
+
+
+galleryContainer.addEventListener("click", onClickModal);
+
+function onClickModal(event) { 
+
+    const imageRealSize = basicLightbox.create(`
+    <div class="modal">
+       <img width="800" height="600"
+            src="${event.target.dataset.source}"
+        />
+    </div>`
+    );
+    
+    imageRealSize.show()
+}
+
 
 
 
 
 // const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <p>
-//             Your first lightbox with just a few lines of code.
-//             Yes, it's really that simple.
-//         </p>
-//     </div>
+//     <img src="https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg" width="800" height="600">
 // `)
 
-// instance.show() 
+// instance.show()
+
+
+
+
+
+
+// On galleryItem click - v1 - 18:00
+// 20:48 - 12-14
